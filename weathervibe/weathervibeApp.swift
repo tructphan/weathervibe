ebcb74e97bf5528a7f6ada7eedcd4c60088031d6
Truc Phan <pttruc.308@gmail.com>
2021-09-04 23:49:31 -0700

    project creation

---
+++ weathervibe/weathervibeApp.swift
@@ -0,0 +1,17 @@
+//
+//  weathervibeApp.swift
+//  weathervibe
+//
+//  Created by Truc Phan on 9/4/21.
+//
+
+import SwiftUI
+
+@main
+struct weathervibeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
